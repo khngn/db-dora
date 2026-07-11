3d3c77a053a5d99ed094bc1eab08dc26c2f3e800
kimhai <kimhai@gmail.com>
2026-07-11 10:07:59 +1000

    Init db-dora-jsb

---
+++ db-dora-jsb/src/main/java/kn/jdb/DbBrowserApplication.java
@@ -0,0 +1,13 @@
+package kn.jdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbBrowserApplication.class, args);
+	}
+
+}
---
+++ db-dora-jsb/src/test/java/kn/jdb/DbBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package kn.jdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
